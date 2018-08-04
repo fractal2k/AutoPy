2525ee51b237724cf2cc0fcc3b40ba3fb68daacd
fractal2k <37149174+fractal2k@users.noreply.github.com>
2018-08-04 22:34:08 +0530

    Added more functionality
    Opens course material in the same window, instead of a new tab

--- AutoPy_Firefox.py
+++ AutoPy_Firefox.py
@@ -8,30 +8,30 @@ import config
 
 class AutoPyFirefox:
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, view_id):
         self.username = username
         self.password = password
-        self.url = f"http://mydy.dypatil.edu/rait/login/index.php?uname={username}&wantsurl="
-        self.browser = webdriver.Firefox()
+        self.url = f"http://mydy.dypatil.edu/rait/login/index.php?uname={username}&wantsurl=http://mydy.dypatil.edu/rait/course/view.php?id={view_id}"
+        self.driver = webdriver.Firefox()
         self.delay = 5
 
     def login(self):
-        self.browser.get(url=self.url)
-        passw = self.browser.find_element_by_id("password")
+        self.driver.get(url=self.url)
+        passw = self.driver.find_element_by_id("password")
         passw.send_keys(self.password)
-        self.browser.find_element_by_id("loginbtn").click()
+        self.driver.find_element_by_id("loginbtn").click()
 
         try:
-            wait = WebDriverWait(self.browser, self.delay)
-            wait.until(EC.presence_of_element_located((By.ID, "inst986")))
+            wait = WebDriverWait(self.driver, self.delay)
+            wait.until(EC.presence_of_element_located((By.ID, "gridiconcontainer")))
         except TimeoutException:
             print("Page timed out")
 
-    def math_launch(self):
-        self.browser.find_element_by_link_text('Launch').click()
+    def click_on_shit(self):
+        self.login()
+        self.driver.find_element_by_xpath('//*[contains(@aria-label, "Section 1")]').click()
 
 
-auto = AutoPyFirefox(config.DY_USERNAME, config.DY_PASSWORD)
-auto.login()
-auto.math_launch()
+auto = AutoPyFirefox(config.USERNAME, config.PASSWORD, config.VIEW_ID_MATH)
+auto.click_on_shit()
 
