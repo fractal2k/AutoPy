8df46d57f16ca0ef70843290adb51dd80a172bf3
fractal2k <37149174+fractal2k@users.noreply.github.com>
2018-08-04 15:13:30 +0530

    Added new functionality
    Auto opens the first course material

--- AutoPy_Firefox.py
+++ AutoPy_Firefox.py
@@ -1,5 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 import config
+
 
 class AutoPyFirefox:
 
@@ -8,14 +13,25 @@ class AutoPyFirefox:
         self.password = password
         self.url = f"http://mydy.dypatil.edu/rait/login/index.php?uname={username}&wantsurl="
         self.browser = webdriver.Firefox()
+        self.delay = 5
 
     def login(self):
         self.browser.get(url=self.url)
         passw = self.browser.find_element_by_id("password")
-
         passw.send_keys(self.password)
         self.browser.find_element_by_id("loginbtn").click()
+
+        try:
+            wait = WebDriverWait(self.browser, self.delay)
+            wait.until(EC.presence_of_element_located((By.ID, "inst986")))
+        except TimeoutException:
+            print("Page timed out")
+
+    def math_launch(self):
+        self.browser.find_element_by_link_text('Launch').click()
 
 
 auto = AutoPyFirefox(config.DY_USERNAME, config.DY_PASSWORD)
 auto.login()
+auto.math_launch()
+
