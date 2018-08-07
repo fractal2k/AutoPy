d6b74e1bbe100d75d7e35857c0d59bbd1f76b4d0
fractal2k <37149174+fractal2k@users.noreply.github.com>
2018-08-07 23:15:58 +0530

    Added functionality
    Script can now open all the links in a section and iterate through all the sections and do the same thing

--- AutoPy_Firefox.py
+++ AutoPy_Firefox.py
@@ -3,6 +3,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 import config
 
 
@@ -18,6 +20,7 @@ class AutoPyFirefox:
     def login(self):
         self.driver.get(url=self.url)
         passw = self.driver.find_element_by_id("password")
+        passw.clear()
         passw.send_keys(self.password)
         self.driver.find_element_by_id("loginbtn").click()
 
@@ -30,12 +33,21 @@ class AutoPyFirefox:
     def click_on_shit(self):
         self.login()
         num_sections = int(len(self.driver.find_elements_by_xpath('//li[contains(@aria-label, "Section")]')) / 2)
-
         self.driver.find_element_by_xpath('//li[contains(@aria-label, "Section")]').click()
+
         for i in range(num_sections - 1):
+            element_parent = self.driver.find_element_by_id('gtopics')
+            element = element_parent.find_element_by_id(f'section-{i + 1}')
+            temp = element.find_elements_by_xpath('.//li[contains(@id, "module-")]')
+
+            for j in temp:
+                child = j.find_element_by_xpath('.//a[contains(@href, "http")]')
+                ActionChains(self.driver).key_down(Keys.LEFT_CONTROL).click(child).key_up(Keys.LEFT_CONTROL).perform()
+
             self.driver.find_element_by_xpath('//*[@id="gridshadebox_right"]').click()
 
 
 auto = AutoPyFirefox(config.USERNAME, config.PASSWORD, config.VIEW_ID_MATH)
 auto.click_on_shit()
+
 
