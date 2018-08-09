edd60bf50fb09a1540923a97f03c2473d856968f
tasuni <37149829+tasuni@users.noreply.github.com>
2018-08-10 01:07:15 +0530

    made code compatible with existing config.py

--- AutoPy_Chrome.py
+++ AutoPy_Chrome.py
@@ -6,14 +6,14 @@ from selenium.webdriver.common.action_chains import ActionChains
 import config
 
 #Opening browser window for the first time, logging in
-user = config.DY_USERNAME
-chromedriver = "Z:\code I'll never see again\Chrome Driver\chromedriver.exe"
+user = config.USERNAME
+chromedriver = "" #put your chromedriver.exe location here
 url = f"http://mydy.dypatil.edu/rait/login/index.php?uname={user}&wantsurl="
 driver = webdriver.Chrome(chromedriver)
 driver.get(url)
 elem = driver.find_element_by_id("password")
 elem.clear()
-elem.send_keys(config.DY_PASSWORD)
+elem.send_keys(config.PASSWORD)
 elem.send_keys(Keys.RETURN)
 
 #initializing the subject buttons' list for the first time
@@ -46,6 +46,6 @@ for i in range(len(subjects)):
     driver.get(url)
     elem = driver.find_element_by_id("password")
     elem.clear()
-    elem.send_keys(config.DY_PASSWORD)
+    elem.send_keys(config.PASSWORD)
     elem.send_keys(Keys.RETURN)
     subjects = driver.find_elements_by_link_text("Launch")
